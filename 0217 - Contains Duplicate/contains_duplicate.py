7021f1b087d9ea8fc70b547ed0d8475e955bb9c7
ElectricGrizzly <26325436+ElectricGrizzly@users.noreply.github.com>
2024-10-31 21:37:40 +0000

    More pythonic type hints

--- 0217 - Contains Duplicate/contains_duplicate.py
+++ 0217 - Contains Duplicate/contains_duplicate.py
@@ -1,11 +1,10 @@
-from typing import List
 from unittest import main, TestCase
 
 
 class Solution:
     # LeetCode annoyingly doesn't follow PEP 8 function naming
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        number_set: set = set()
+    def containsDuplicate(self, nums: list[int]) -> bool:
+        number_set: set[int] = set()
         for number in nums:
             if number in number_set:
                 return True
@@ -14,15 +13,15 @@ class Solution:
 
 class TestContainsDuplicate(TestCase):
     def test_when_duplicate_in_small_list(self):
-        numbers: List[int] = [1, 2, 3, 1]
+        numbers: list[int] = [1, 2, 3, 1]
         self.assertTrue(Solution().containsDuplicate(numbers))
     
     def test_when_duplicate_in_large_list(self):
-        numbers: List[int] = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
+        numbers: list[int] = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
         self.assertTrue(Solution().containsDuplicate(numbers))
 
     def test_when_no_duplicate_in_small_list(self):
-        numbers: List[int] = [1, 2, 3, 4]
+        numbers: list[int] = [1, 2, 3, 4]
         self.assertFalse(Solution().containsDuplicate(numbers))
 
 if __name__ == '__main__':
